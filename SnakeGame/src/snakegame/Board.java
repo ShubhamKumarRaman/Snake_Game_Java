be9ba2d207ae36d5db2d79ab850089567c4378b8
ShubhamKumarRaman <shubham620409@gmail.com>
2025-02-13 22:27:48 +0530

    Update Board.java

--- SnakeGame/src/snakegame/Board.java
+++ SnakeGame/src/snakegame/Board.java
@@ -1,11 +1,12 @@
 package snakegame;
 
+import java.awt.Color;
 import javax.swing.JPanel;
 
 public class Board extends JPanel {
 
     public Board() {
-    
+        setBackground(new Color.Black);
     }
     
 }
