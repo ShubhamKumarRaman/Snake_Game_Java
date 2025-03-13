d5fe2a39d8aee39fcd5e32a99c0556396843fe4f
ShubhamKumarRaman <shubham620409@gmail.com>
2025-03-13 15:24:32 +0530

    Update Board.java

--- SnakeGame/src/snakegame/Board.java
+++ SnakeGame/src/snakegame/Board.java
@@ -1,20 +1,33 @@
 package snakegame;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class Board extends JPanel {
+public class Board extends JPanel implements ActionListener {
 
     private Image apple;
     private Image dot;
     private Image head;
 
-    private final int x[] = new int[];
-    private final int y[] = new int[];
+    private final int ALL_DOTS = 250000;
+    private final int DOT_SIZE = 10;
+
+    private final int RANDOM_POSITION = 49;
+    private int apple_x;
+    private int apple_y;
+
+    private final int x[] = new int[ALL_DOTS];
+    private final int y[] = new int[ALL_DOTS];
 
     private int dots;
+    private Timer timer;
 
     public Board() {
         setBackground(Color.BLACK);
@@ -37,7 +50,44 @@ public class Board extends JPanel {
         dots = 3;
 
         for (int i = 0; i < dots; i++) {
-
+            y[i] = 50;
+            x[i] = 50 - i * DOT_SIZE;
         }
+
+        locateApple();
+
+        timer = new Timer(140, this);
+        timer.start();
+    }
+
+    public void locateApple() {
+        int r = (int) (Math.random() * RANDOM_POSITION);
+        apple_x = r * DOT_SIZE;
+        r = (int) (Math.random() * RANDOM_POSITION);
+        apple_y = r * DOT_SIZE;
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        g.drawImage(apple, apple_x, apple_y, this);
+
+        for (int i = 0; i < dots; i++) {
+            if (i == 0) {
+                g.drawImage(head, x[i], y[i], this);
+            } else {
+                g.drawImage(dot, x[i], y[i], this);
+            }
+        }
+
+        Toolkit.getDefaultToolkit().sync();
+    }
+    
+    public void actionPerformed(ActionEvent ae){
+        
     }
 }
